5637105ef056255e3e1381331e08ae162b6e02cb
spooee <32413258+spooee@users.noreply.github.com>
2023-11-24 22:05:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Auto Bunnyhop
+Simple plugin that spams Spacebar for you while Spacebar is held down in Final Fantasy XIV.
+
+## Installation
+- Type ```/xlsettings``` in chat
+- Head to the "Experimental" tab
+- Add ```https://raw.githubusercontent.com/spooee/ffxiv_plogons/master/pluginmaster.json``` into an empty box under "Custom Plugin Repositories"
+- Hit the "+" button
+- Press the "Save changes and close" button located in the bottom right corner of the window
+- Install the plugin like you would any other plugin in the Plugin Installer and enjoy
---
+++ autobhop/Plugin.cs
@@ -0,0 +1,58 @@
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using ECommons;
+using Dalamud.Game.ClientState.Conditions;
+using System.Windows.Forms;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace autobhop
+{
+    public sealed class Plugin : IDalamudPlugin
+    {
+        private DalamudPluginInterface PluginInterface { get; init; }
+        private ICommandManager CommandManager { get; init; }
+        private bool InFlight => Service.Condition[ConditionFlag.InFlight];
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        private readonly IntPtr ffxivWindowHandle;
+
+        public Plugin(
+            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+            [RequiredVersion("1.0")] ICommandManager commandManager)
+        {
+            this.PluginInterface = pluginInterface;
+            this.CommandManager = commandManager;
+            this.PluginInterface.Create<Service>(this);
+            ECommonsMain.Init(pluginInterface, this, Module.All);
+
+            this.ffxivWindowHandle = Process.GetCurrentProcess().MainWindowHandle;
+
+            Service.Framework.Update += onFrameworkUpdate;
+        }
+
+        public void onFrameworkUpdate(IFramework framework)
+        {
+            IntPtr foregroundWindow = GetForegroundWindow();
+            if (foregroundWindow == this.ffxivWindowHandle)
+            {
+                if (GenericHelpers.IsKeyPressed(Keys.Space) && !InFlight)
+                {
+                    ECommons.Automation.WindowsKeypress.SendKeypress(Keys.Space);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Service.Framework.Update -= onFrameworkUpdate;
+        }
+    }
+}
